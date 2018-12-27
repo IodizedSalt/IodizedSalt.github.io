8e858ee9058e20cc963b3c7d8cf2b899f4d970b3
IodizedSalt <christophermoeller2000@gmail.com>
2018-12-27 17:13:28 +0100

    refactor for prod and dev to mirror output

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require("path")
 const port = 3000
 
 
-app.use(express.static(path.join(__dirname, '/.', 'public')));
+app.use(express.static(path.join(__dirname, '/.', '')));
 
 
 app.get('/', function(req, res){
