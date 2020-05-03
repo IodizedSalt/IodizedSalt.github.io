f1af7ee768318f036a3c0f60645e131da5c7bece
IodizedSalt <christophermoeller2000@gmail.com>
2020-05-03 17:52:47 +0200

    photo editor project

--- js/slide.js
+++ js/slide.js
@@ -16,3 +16,22 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block";
 }
+
+
+var slideIndex = 1;
+showDivs2(slideIndex);
+
+function plusDivs2(n) {
+  showDivs2(slideIndex += n);
+}
+
+function showDivs2(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides2");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
