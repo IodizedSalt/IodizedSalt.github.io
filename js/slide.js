21ab28999c89eb2b5f8e2cebcc572f0517073c6c
IodizedSalt <christophermoeller2000@gmail.com>
2020-05-03 19:00:43 +0200

    webshop

--- js/slide.js
+++ js/slide.js
@@ -35,3 +35,21 @@ function showDivs2(n) {
   }
   x[slideIndex-1].style.display = "block";
 }
+
+var slideIndex = 1;
+showDivs3(slideIndex);
+
+function plusDivs3(n) {
+  showDivs3(slideIndex += n);
+}
+
+function showDivs3(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides3");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
